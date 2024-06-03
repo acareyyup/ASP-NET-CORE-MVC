c76c4f20393ca6c4c48adc071e39a0ce6a323acf
Eyyüp Acar <100216722+acareyyup@users.noreply.github.com>
2024-06-03 18:19:13 +0300

    Product Controller ve Product Viewler oluşturuldu

---
+++ StoreApp/Controllers/ProductController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StoreApp.Models;
+
+namespace StoreApp.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly RepositoryContext _repositoryContext;
+
+        public ProductController(RepositoryContext repositoryContext)
+        {
+            _repositoryContext = repositoryContext;
+        }
+
+        public IActionResult Index()
+        {
+            var model = _repositoryContext.Products.ToList();
+            return View(model);
+        }
+        public IActionResult Get(int id)
+        {
+            Product product = _repositoryContext.Products.First(p => p.Id == id);
+            return View(product);
+        }
+
+
+    }
+}
