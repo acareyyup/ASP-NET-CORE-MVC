3748dc6be6ea3d16bc5e1c728f8f402d0a5917aa
Eyyüp Acar <100216722+acareyyup@users.noreply.github.com>
2024-06-04 18:42:43 +0300

    Contoller Views ve Layout oluşturuldu

---
+++ StoreApp/Areas/Admin/Controllers/DashboardController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace StoreApp.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class DashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            TempData["info"] = $"Welcome back, {DateTime.Now.ToShortTimeString()}";
+            return View();
+        }
+    }
+}
