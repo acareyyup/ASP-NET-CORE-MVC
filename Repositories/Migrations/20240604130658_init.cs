95a332ec005e1f4dc4201090222a54f49d768e56
Eyyüp Acar <100216722+acareyyup@users.noreply.github.com>
2024-06-04 16:10:12 +0300

    Repositories Katmanı Oluşturuldu

---
+++ Repositories/Migrations/20240604130658_init.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Repositories.Migrations
+{
+    public partial class init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ProductName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Price = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Price", "ProductName" },
+                values: new object[,]
+                {
+                    { 1, 17500m, "Computer" },
+                    { 2, 100m, "Keyboard" },
+                    { 3, 500m, "Mouse" },
+                    { 4, 7000m, "Monitor" },
+                    { 5, 1500m, "Deck" },
+                    { 6, 2000m, "Camera" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Products");
+        }
+    }
+}
---
+++ Repositories/RepositoryContext.cs
@@ -0,0 +1,30 @@
+﻿using Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Repositories
+{
+    public class RepositoryContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+        public RepositoryContext(DbContextOptions<RepositoryContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Product>()
+                .HasData(
+                new Product() { Id = 1, ProductName = "Computer", Price = 17_500 },
+                new Product() { Id = 2, ProductName = "Keyboard", Price = 1_00 },
+                new Product() { Id = 3, ProductName = "Mouse", Price = 500 },
+                new Product() { Id = 4, ProductName = "Monitor", Price = 7_000 },
+                new Product() { Id = 5, ProductName = "Deck", Price = 1_500 },
+                new Product() { Id = 6, ProductName = "Camera", Price = 2_000 }
+                );
+        }
+
+    }
+}
