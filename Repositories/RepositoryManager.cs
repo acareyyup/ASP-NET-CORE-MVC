2f68c6e7710aeafc601b87a791fe857bace5c0c8
Eyyüp Acar <100216722+acareyyup@users.noreply.github.com>
2024-06-04 16:42:40 +0300

    Repositories katmanıyla ilgili Interfaceler,Base ve Managerler oluşturuldu

---
+++ Repositories/RepositoryManager.cs
@@ -0,0 +1,23 @@
+﻿using Repositories.Contracts;
+
+namespace Repositories
+{
+    public class RepositoryManager : IRepositoryManager
+    {
+        private readonly RepositoryContext _context;
+        private readonly IProductRepository _productRepository;
+
+        public RepositoryManager(RepositoryContext context, IProductRepository productRepository)
+        {
+            _context = context;
+            _productRepository = productRepository;
+        }
+
+        public IProductRepository Product => _productRepository;
+        public void Save()
+        {
+            _context.SaveChanges();
+        }
+
+    }
+}
