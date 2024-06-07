83e91a0be098a3f03389a02e24546aa557478f52
Eyyüp Acar <100216722+acareyyup@users.noreply.github.com>
2024-06-07 19:48:05 +0300

    HttpRequest Extension oluşturuldu

--- StoreApp/Pages/Cart.cshtml.cs
+++ StoreApp/Pages/Cart.cshtml.cs
@@ -25,11 +25,11 @@ namespace StoreApp.Pages
             // Cart = HttpContext.Session.GetJson<Cart>("cart") ?? new Cart();
         }
 
-        public IActionResult OnPost(int productId, string returnUrl)
+        public IActionResult OnPost(int Id, string returnUrl)
         {
             Product? product = _manager
                 .ProductService
-                .GetOneProduct(productId, false);
+                .GetOneProduct(Id, false);
 
             if (product is not null)
             {
