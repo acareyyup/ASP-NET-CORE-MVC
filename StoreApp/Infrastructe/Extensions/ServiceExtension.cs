99baacdaf33bdcd2c2f6689e2a0a2cfd37fb5462
Eyyüp Acar <100216722+acareyyup@users.noreply.github.com>
2024-06-09 17:51:38 +0300

    ConfigureSession,ConfigureRepositoryRegistration,ConfigureServiceRegistration ve ConfigureRouting Extensionları yapılıp Program.cs düzeltildi

--- StoreApp/Infrastructe/Extensions/ServiceExtension.cs
+++ StoreApp/Infrastructe/Extensions/ServiceExtension.cs
@@ -22,5 +22,42 @@ namespace StoreApp.Infrastructe.Extensions
                 });
             });
         }
+
+        public static void ConfigureSession(this IServiceCollection services)
+        {
+            services.AddDistributedMemoryCache();
+            services.AddSession(options =>
+            {
+                options.Cookie.Name = "StoreApp.Session";
+                options.IdleTimeout = TimeSpan.FromMinutes(10);
+            });
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            services.AddScoped<Cart>(c => SessionCart.GetCart(c));
+        }
+
+        public static void ConfigureRepositoryRegistration(this IServiceCollection services)
+        {
+            services.AddScoped<IRepositoryManager, RepositoryManager>();
+            services.AddScoped<IProductRepository, ProductRepository>();
+            services.AddScoped<ICategoryRepository, CategoryRepository>();
+            services.AddScoped<IOrderRepository, OrderRepository>();
+        }
+
+        public static void ConfigureServiceRegistration(this IServiceCollection services)
+        {
+            services.AddScoped<IServiceManager, ServiceManager>();
+            services.AddScoped<IProductService, ProductManager>();
+            services.AddScoped<ICategoryService, CategoryManager>();
+            services.AddScoped<IOrderService, OrderManager>();
+        }
+
+        public static void ConfigureRouting(this IServiceCollection services)
+        {
+            services.AddRouting(options =>
+            {
+                options.LowercaseUrls = true;
+                options.AppendTrailingSlash = false;
+            });
+        }
     }
 }
--- StoreApp/Program.cs
+++ StoreApp/Program.cs
@@ -1,10 +1,4 @@
-using Entities.Models;
-using Repositories;
-using Repositories.Contracts;
-using Services;
-using Services.Contracts;
 using StoreApp.Infrastructe.Extensions;
-using StoreApp.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,34 +6,18 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
 
 builder.Services.ConfigureDbContext(builder.Configuration);
-
-builder.Services.AddDistributedMemoryCache();
-builder.Services.AddSession(options =>
-{
-    options.Cookie.Name = "StoreApp.Session";
-    options.IdleTimeout = TimeSpan.FromMinutes(10);
-});
-builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
-
-builder.Services.AddScoped<IRepositoryManager, RepositoryManager>();
-builder.Services.AddScoped<IProductRepository, ProductRepository>();
-builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
-builder.Services.AddScoped<IOrderRepository, OrderRepository>();
-
-builder.Services.AddScoped<IServiceManager, ServiceManager>();
-builder.Services.AddScoped<IProductService, ProductManager>();
-builder.Services.AddScoped<ICategoryService, CategoryManager>();
-builder.Services.AddScoped<IOrderService, OrderManager>();
-
-builder.Services.AddScoped<Cart>(c => SessionCart.GetCart(c));
+builder.Services.ConfigureSession();
+builder.Services.ConfigureRepositoryRegistration();
+builder.Services.ConfigureServiceRegistration();
+builder.Services.ConfigureRouting();
 
 builder.Services.AddAutoMapper(typeof(Program));
-
 
 var app = builder.Build();
 
 app.UseStaticFiles();
 app.UseSession();
+
 app.UseHttpsRedirection();
 app.UseRouting();
 
@@ -52,7 +30,7 @@ app.UseEndpoints(endpoints =>
     );
 
     endpoints.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
+
     endpoints.MapRazorPages();
 });
-
 app.Run();
