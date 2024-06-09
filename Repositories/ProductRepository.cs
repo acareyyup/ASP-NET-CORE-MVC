96255974597ef84d6c9f8bcf427f877dc1d8fbd5
Eyyüp Acar <100216722+acareyyup@users.noreply.github.com>
2024-06-09 19:04:29 +0300

    Filter Product Search,Price ve Kategori ID göre Extensionlar oluşturuldu

--- Repositories/ProductRepository.cs
+++ Repositories/ProductRepository.cs
@@ -2,10 +2,11 @@
 using Entities.RequestParameters;
 using Microsoft.EntityFrameworkCore;
 using Repositories.Contracts;
+using Repositories.Extensions;
 
 namespace Repositories
 {
-    public class ProductRepository : RepositoryBase<Product>, IProductRepository
+    public sealed class ProductRepository : RepositoryBase<Product>, IProductRepository
     {
         public ProductRepository(RepositoryContext context) : base(context)
         {
@@ -14,14 +15,11 @@ namespace Repositories
         public IQueryable<Product> GetAllProducts(bool trackChanges) => FindAll(trackChanges);
         public IQueryable<Product> GetAllProductsWithDetails(ProductRequestParameters p)
         {
-            return p.CategoryId is null
-                ? _context
+            return _context
                 .Products
-                .Include(prd => prd.Category)
-                : _context
-                .Products
-                .Include(prd => prd.Category)
-                .Where(prd => prd.CategoryId.Equals(p.CategoryId));
+                .FilteredByCategoryId(p.CategoryId)
+                .FilteredBySearchTerm(p.SearchTerm)
+                .FilteredByPrice(p.MinPrice, p.MaxPrice, p.IsValidPrice);
         }
         public IQueryable<Product> GetShowcaseProducts(bool trackChanges)
         {
