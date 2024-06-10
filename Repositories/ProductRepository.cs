3d1b31f965fd51b1f32e2e80a49d99a190d536e4
Eyyüp Acar <100216722+acareyyup@users.noreply.github.com>
2024-06-10 10:25:05 +0300

    Pagination PageLinkTagHelper uygulandı

--- Repositories/ProductRepository.cs
+++ Repositories/ProductRepository.cs
@@ -1,6 +1,5 @@
 ﻿using Entities.Models;
 using Entities.RequestParameters;
-using Microsoft.EntityFrameworkCore;
 using Repositories.Contracts;
 using Repositories.Extensions;
 
@@ -10,29 +9,38 @@ namespace Repositories
     {
         public ProductRepository(RepositoryContext context) : base(context)
         {
+
         }
 
+        public void CreateOneProduct(Product product) => Create(product);
+
+        public void DeleteOneProduct(Product product) => Remove(product);
+
         public IQueryable<Product> GetAllProducts(bool trackChanges) => FindAll(trackChanges);
+
         public IQueryable<Product> GetAllProductsWithDetails(ProductRequestParameters p)
         {
             return _context
                 .Products
                 .FilteredByCategoryId(p.CategoryId)
                 .FilteredBySearchTerm(p.SearchTerm)
-                .FilteredByPrice(p.MinPrice, p.MaxPrice, p.IsValidPrice);
+                .FilteredByPrice(p.MinPrice, p.MaxPrice, p.IsValidPrice)
+                .ToPaginate(p.PageNumber, p.PageSize);
         }
+
+        // Interface
+        public Product? GetOneProduct(int id, bool trackChanges)
+        {
+            return FindByCondition(p => p.Id.Equals(id), trackChanges);
+        }
+
         public IQueryable<Product> GetShowcaseProducts(bool trackChanges)
         {
             return FindAll(trackChanges)
                 .Where(p => p.ShowCase.Equals(true));
         }
 
-        public Product? GetOneProduct(int id, bool trackChanges)
-        {
-            return FindByCondition(p => p.Id.Equals(id), trackChanges);
-        }
-        public void DeleteOneProduct(Product product) => Remove(product);
-        public void UpdateOneProduct(Product product) => Update(product);
+        public void UpdateOneProduct(Product entity) => Update(entity);
 
     }
 }
--- StoreApp/Controllers/ProductController.cs
+++ StoreApp/Controllers/ProductController.cs
@@ -1,6 +1,10 @@
+using Entities.Models;
 using Entities.RequestParameters;
 using Microsoft.AspNetCore.Mvc;
+using Repositories;
+using Repositories.Contracts;
 using Services.Contracts;
+using StoreApp.Models;
 
 namespace StoreApp.Controllers
 {
@@ -15,8 +19,18 @@ namespace StoreApp.Controllers
 
         public IActionResult Index(ProductRequestParameters p)
         {
-            var model = _manager.ProductService.GetAllProductsWithDetails(p);
-            return View(model);
+            var products = _manager.ProductService.GetAllProductsWithDetails(p);
+            var pagination = new Pagination()
+            {
+                CurrenPage = p.PageNumber,
+                ItemsPerPage = p.PageSize,
+                TotalItems = _manager.ProductService.GetAllProducts(false).Count()
+            };
+            return View(new ProductListViewModel()
+            {
+                Products = products,
+                Pagination = pagination
+            });
         }
 
         public IActionResult Get([FromRoute(Name = "id")] int id)
