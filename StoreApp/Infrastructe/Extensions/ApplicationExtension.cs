b96347af759a8c4f0aebbef4b6b624589952e084
Eyyüp Acar <100216722+acareyyup@users.noreply.github.com>
2024-06-10 11:34:40 +0300

    Default Admin User Oluşturuldu

--- StoreApp/Infrastructe/Extensions/ApplicationExtension.cs
+++ StoreApp/Infrastructe/Extensions/ApplicationExtension.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Repositories;
 
 namespace StoreApp.Infrastructe.Extensions
@@ -27,6 +28,51 @@ namespace StoreApp.Infrastructe.Extensions
                     .AddSupportedUICultures("tr-TR")
                     .SetDefaultCulture("tr-TR");
             });
+        }
+        public static async void ConfigureDefaultAdminUser(this IApplicationBuilder app)
+        {
+            const string adminUser = "Admin";
+            const string adminPassword = "Admin.123456";
+
+            // UserManager
+            UserManager<IdentityUser> userManager = app
+                .ApplicationServices
+                .CreateScope()
+                .ServiceProvider
+                .GetRequiredService<UserManager<IdentityUser>>();
+
+            // RoleManager
+            RoleManager<IdentityRole> roleManager = app
+                .ApplicationServices
+                .CreateAsyncScope()
+                .ServiceProvider
+                .GetRequiredService<RoleManager<IdentityRole>>();
+
+            IdentityUser user = await userManager.FindByNameAsync(adminUser);
+            if (user is null)
+            {
+                user = new IdentityUser()
+                {
+                    Email = "Admin@gmail.com",
+                    PhoneNumber = "5061112233",
+                    UserName = adminUser,
+                };
+
+                var result = await userManager.CreateAsync(user, adminPassword);
+
+                if (!result.Succeeded)
+                    throw new Exception("Admin user could not created.");
+
+                var roleResult = await userManager.AddToRolesAsync(user,
+                    roleManager
+                        .Roles
+                        .Select(r => r.Name)
+                        .ToList()
+                );
+
+                if (!roleResult.Succeeded)
+                    throw new Exception("System have problems with role defination for admin.");
+            }
         }
     }
 }
---
+++ StoreApp/Migrations/20240610082513_IdentityRoleSeedData.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StoreApp.Migrations
+{
+    public partial class IdentityRoleSeedData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "0470f42b-f7ea-4994-af34-6dc651d04156", "6e0b0ad1-57b1-4c39-824e-e8e250ac40cd", "Editor", "EDITOR" });
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "3d4074e4-6350-4ed5-b30a-7a793ebdfa9a", "a12dddfb-b8ef-4d6b-abb0-cd3742407421", "Admin", "ADMIN" });
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "767125aa-aaab-41b0-9e48-838706bf2763", "ef195e7c-4505-43c1-98ae-e606bfcc70ba", "User", "USER" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "0470f42b-f7ea-4994-af34-6dc651d04156");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "3d4074e4-6350-4ed5-b30a-7a793ebdfa9a");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "767125aa-aaab-41b0-9e48-838706bf2763");
+        }
+    }
+}
